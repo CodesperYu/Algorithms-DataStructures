b281db78e8ba3a31c8351134ea07b5dd38b093b7
Jasper Yu <Jasperyu.kk@gmail.com>
2018-12-03 22:37:41 -0800

    test for LIS

---
+++ Algorithms/Dynamic Programming/LIS/LIS.test.js
@@ -0,0 +1,15 @@
+const LIS = require('./LIS');
+
+test('standard test', () => {
+	let input = [10, 22, 9 ,33, 21, 50, 41, 60, 80]  
+	let answer = [10, 22, 33, 50, 60, 80]
+
+	let result = LIS(input);
+	expect(result).toEqual(answer);
+});
+
+test('test with larger gap', ()=>{
+	let input = [10,22,9,33,23,50,41,52,53,60,80,24,25,226,27,28,29,30,31,32,33,34,29,30,31,32,33,34];
+	let answer = []
+
+});
