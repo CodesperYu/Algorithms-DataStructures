7a4b4ffe12f76728e13cc04411a9a9bb5076703b
Jasper Yu <Jasperyu.kk@gmail.com>
2019-03-15 13:04:50 -0700

    search api

--- Data Structures/LinkedList/MergeMultipleList.js
+++ Data Structures/LinkedList/MergeMultipleList.js
@@ -1,47 +1,44 @@
-let mergeKLists = function (lists) {
-  let mergedLinkList = null
+const mergeKLists = function (lists) {
+  let mergedLinkList = null;
   for (let i = 0; i < lists.length; i++) {
     if (mergedLinkList === null) {
-      mergedLinkList = lists[i]
+      mergedLinkList = lists[i];
     } else {
-      mergedLinkList = combineTwoSortedLists(mergedLinkList, lists[i])
+      mergedLinkList = combineTwoSortedLists(mergedLinkList, lists[i]);
     }
   }
-  return mergedLinkList
-}
+  return mergedLinkList;
+};
 
 let combineTwoSortedLists = (mainList, listToInsert) => {
-  let mainListCurr = mainList
-  let mainListPrev = null
-  let listToInsertCurr = listToInsert
+  let mainListCurr = mainList;
+  let mainListPrev = null;
+  let listToInsertCurr = listToInsert;
 
   while (listToInsertCurr !== null) {
     if (mainListCurr.val >= listToInsertCurr.val) {
-      let temp = listToInsertCurr.next
+      const temp = listToInsertCurr.next;
 
       // reset if you replace the head node
       if (mainListPrev === null) {
-        listToInsertCurr.next = mainListCurr
-        mainList = listToInsertCurr
-        mainListCurr = mainList
+        listToInsertCurr.next = mainListCurr;
+        mainList = listToInsertCurr;
+        mainListCurr = mainList;
         // else change mainlistprev.next to listtoinsertcurr
         // list to insert curr.next
       } else {
-        listToInsertCurr.next = mainListCurr
-        mainListPrev.next = listToInsertCurr
-        mainListCurr = mainListPrev.next
+        listToInsertCurr.next = mainListCurr;
+        mainListPrev.next = listToInsertCurr;
+        mainListCurr = mainListPrev.next;
       }
-      listToInsertCurr = temp
-      console.log(listToInsertCurr)
+      listToInsertCurr = temp;
+    } else if (mainListCurr.next === null) {
+      mainListCurr.next = listToInsertCurr;
+      listToInsertCurr = null;
     } else {
-      if (mainListCurr.next === null) {
-        mainListCurr.next = listToInsertCurr
-        listToInsertCurr = null
-      } else {
-        mainListPrev = mainListCurr
-        mainListCurr = mainListCurr.next
-      }
+      mainListPrev = mainListCurr;
+      mainListCurr = mainListCurr.next;
     }
   }
-  return mainList
-}
+  return mainList;
+};
