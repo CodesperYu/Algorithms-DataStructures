5a4d3a64eced6a98604ddb2bd9d943ad721ba803
Jasper Yu <Jasperyu.kk@gmail.com>
2019-03-29 11:51:43 -0700

    construct max bst

--- Algorithms/Dynamic Programming/sellStock.js
+++ Algorithms/Dynamic Programming/sellStock.js
@@ -3,7 +3,6 @@ const maxProfit = (prices) => {
   let bestSellProfit = 0;
   let profit = 0;
   for (let i = 1; i < prices.length; i++) {
-    console.log(buyPrice, bestSellProfit);
     const currPrice = prices[i];
     const currTradeProfit = currPrice - buyPrice - 2;
     if (bestSellProfit === 0 && currPrice < buyPrice) {
@@ -18,6 +17,17 @@ const maxProfit = (prices) => {
   }
   return profit += bestSellProfit;
 };
+
+// better solution
+// var maxProfit = function(prices, fee) {
+// 	let profit = 0;
+// 	let hold = 0 - prices[0];
+// 	for (let i = 1; i < prices.length; i++) {
+// 			profit = Math.max(profit, hold + prices[i] - fee);
+// 			hold = Math.max(hold, profit - prices[i]);
+// 	}
+// 	return profit
+// };
 
 const test = [1, 3, 2, 8, 4, 10];
 const test2 = [6, 3, 2, 8, 4, 10];
---
+++ Data Structures/BinarySearchTrees/ConstructMaximumBinaryTree.js
@@ -0,0 +1,23 @@
+// Given an integer array with no duplicates. A maximum tree building on this array is defined as follow:
+
+// The root is the maximum number in the array.
+// The left subtree is the maximum tree constructed from left part subarray divided by the maximum number.
+// The right subtree is the maximum tree constructed from right part subarray divided by the maximum number.
+// Construct the maximum tree by the given array and output the root node of this tree.
+
+
+var constructMaximumBinaryTree = function (nums) {
+  if (!nums.length) return null;
+
+  const largest = Math.max(...nums);
+  const largestNumsIndex = nums.indexOf(largest);
+
+  const tree = new TreeNode(largest);
+  const left = nums.slice(0, largestNumsIndex);
+  const right = nums.slice(largestNumsIndex + 1);
+
+  tree.left = constructMaximumBinaryTree(left);
+  tree.right = constructMaximumBinaryTree(right);
+
+  return tree;
+};
